db0a8d9ec7a9ba0cd975864675624527ef3d0a95
Administrator <854982469@qq.com>
2016-08-24 19:28:14 +0800

    主页模块3

---
+++ gouchaolibrary/src/main/java/com/qianfeng/widget/ScrollAbleFragment.java
@@ -0,0 +1,14 @@
+package com.qianfeng.widget;/**
+ * Created by cpoopc on 2015/8/28.
+ */
+
+import android.support.v4.app.Fragment;
+
+/**
+ * User: cpoopc
+ * Date: 2015-08-28
+ * Time: 11:45
+ * Ver.: 0.1
+ */
+public abstract class ScrollAbleFragment extends Fragment implements ScrollableHelper.ScrollableContainer {
+}
